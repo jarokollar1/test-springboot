a94db898b1da461011f07b9380d72600f98dac26
a605053 <jaroslav.kollar@eviden.com>
2024-06-28 10:30:49 +0200

    generated project

---
+++ src/test/java/com/eviden/springboot_sample_rest/SpringbootSampleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviden.springboot_sample_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSampleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
